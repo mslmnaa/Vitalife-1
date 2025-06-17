d922b19e06fc9cb313abcf973fb4ce8183adcb39
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-06-17 12:22:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',  // ubah dari sass ke css
+                'resources/css/app.css',  
                 'resources/js/app.js',
             ],
             refresh: true,
