b8eddbf6f84b3191d6c45cb78340e4430e1341d9
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-06-17 19:27:59 +0700

    a

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     public function Adminhomepage()
     {
 
-        $eventcount = Event::count();
+        // $eventcount = Event::count();
         $spescount = Spesialis::count();
         
         // Get recent payments
