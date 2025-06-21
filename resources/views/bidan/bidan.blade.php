979a7a4114b6ad0cad684cde325bfe5519b8af97
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-06-22 00:59:32 +0700

    asas

--- resources/views/bidan/bidan.blade.php
+++ resources/views/bidan/bidan.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Klinik Ralinish Cikarang Bekasi - Layanan Kesehatan Ibu dan Anak</title>
+    <title>Klinik dayat Ralinish Cikarang Bekasi - Layanan Kesehatan Ibu dan Anak</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
