1aec556d1dd6772424335c833f6be429abf2b0db
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-07-09 00:05:33 +0700

    a

--- resources/views/layouts/doctor.blade.php
+++ resources/views/layouts/doctor.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
     <style>
         .sidebar-transition {
