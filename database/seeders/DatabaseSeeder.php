499d29f1ea74ba33c38e4362343c44a9a8ac0034
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-07-09 00:02:04 +0700

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
 {
     $this->call([
-        UserSeeder::class,
-        SpesialisSeeder::class,
-        MedicineSeeder::class,
-        PharmacySeeder::class,
+        // UserSeeder::class,
+        // SpesialisSeeder::class,
+        // MedicineSeeder::class,
+        // PharmacySeeder::class,
         dummydatauser::class
 
 
