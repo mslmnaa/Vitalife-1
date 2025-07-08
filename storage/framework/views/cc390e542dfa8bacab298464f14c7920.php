f7e47b4f28c5e08de7fd187f4947fe76d2cc15ed
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-07-09 00:10:25 +0700

    a

--- storage/framework/views/cc390e542dfa8bacab298464f14c7920.php
+++ storage/framework/views/cc390e542dfa8bacab298464f14c7920.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
-    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+    
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
     <style>
