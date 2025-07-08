d1b53c8715ac0e90ac401cdb167807e11d73df47
Salman <149822719+mslmnaa@users.noreply.github.com>
2025-07-09 00:25:24 +0700

    a

--- storage/framework/views/cc390e542dfa8bacab298464f14c7920.php
+++ storage/framework/views/cc390e542dfa8bacab298464f14c7920.php
@@ -126,7 +126,7 @@
                     <a href="<?php echo e(route('doctor.patients')); ?>" 
                        class="flex items-center px-4 py-3 text-sm font-medium rounded-lg transition-colors <?php echo e(request()->routeIs('doctor.patients*') ? 'bg-blue-50 text-blue-700 border-r-2 border-blue-700' : 'text-gray-700 hover:bg-gray-100'); ?>">
                         <i class="fas fa-users mr-3 w-5"></i>
-                        My Patientss
+                        My Patientsss
                     </a>
                     
                    
